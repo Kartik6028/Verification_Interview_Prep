ce33d8c42dd2e7ce4b45c5299b74bccebda590cd
kartik saxena <ks6028@nyu.edu>
2026-01-15 04:04:29 -0700

    first uvm cleanup

--- UVM_CODING_TBD.sv
+++ UVM_CODING_TBD.sv
@@ -1,193 +1,3 @@
-// CONCEPT
-    // DownCast & Upcase
-
-    //UPCASTING Treating a child object as parent type
-
-    class base_pkt extends uvm_sequence_item;
-    endclass
-
-    class burst_pkt extends base_pkt;
-    endclass
-
-    burst_pkt bp;
-    base_pkt p;
-
-    bp = new("bp");
-    p = bp; //upcasting
-
-    uvm_component comps [$];
-    drv = my_driver::type_id::create ("drv");
-    comps.pushback(drv);
-
-    /*
-    why do we ever need this ?
-    ans - for a number of reasons
-    1) you can use a generic base component handle and still be able to call the most derived method.
-    2) factory always returns the base handle type but the actual object is derived type.
-    3) phase execution
-    4) reuseablitiy
-    */
-
-
-    // DURING UPCASTING YOU CANNOT USE A FUNCTION THAT IS EXCLUSIVELY IN THE DERIVED CLASS AND NOT OVERRIDEEN for that we need to downcast.
-    // DOWNCAST Gives you the ability to check that whether this parent handle was upcasted before, ie does it really point to a child? if yes then you can safely use all the derived-only methods from the child
-    p = burst_pkt::type_id::create("p");
-    bp = p // illegal in SV
-    if (!$cast (bp, p))
-    begin
-        `uvm_error("nope")
-    end
-
-
-    class parent;
-        var x;
-        virtual function vir;
-        // some code
-        endfunction
-
-        function non_vir_parent
-        endfunction
-    endclass
-
-    class child extends from parent;
-        var y;
-        virtual fuction vir;
-        //somecode
-        endfunction
-
-        function non_vir_parent
-        endfunction
-
-        function fun_child;
-       // some code
-        endfunction
-    endclass
-
-
-    parent p;
-    child c;
-    c = new ("c");
-    p =c  //  UPCASTING
-    /*
-    now this means that p.vir would actually execute function from child and not parent.
-    but p.non_vir_parent will actually execute that function from parent.
-    and p.fun_child is impossible as it is derived only ?
-    and p.y can we use this var from child - cannot
-    */
-    parent p;
-    child c;
-
-    $cast(c,p); // downcasting
-    // ye cast do kaam karta hai, 1) check karega ki parent kya sach me child ko point karta hai ? agaar haan to tumhe ek child ka handle dedega jo seedha child ko point karta hai to use child only methods.
-    //then now if i do p.vir - same as before child's derived function, but now i can also do c.fun_child or access c.y which are both child only functions right ?
-//CONCEPT
-
-interface mem_if (input clk, reset);
-
-    logic [1:0] addr;
-    logic wr_en;
-    logic rd_en;
-    logic [7:0]wdata;
-    logic [7:0]rdata;
-
-
-    clocking driver_cb @(posedge clk);
-    default input #1 output #1;
-    output addr;
-    output wr_enl
-    output wdata;
-    output rd_en;
-    input rdata;
-    endclocking
-
-
-    clocking monitor_cb @(posedge clk);
-        default input #1 output #1;
-        input addr;
-        input wr_enl
-        input wdata;
-        input rd_en;
-        input rdata;
-    endclocking
-
-    modport DRIVER (clocking driver_cb,input clk, reset);
-    modport MONITOR (clocking monitor_cb,input clk, reset);
-endinterface
-
-module tb_top;
-    bit clk;
-    bit reset;
-
-    always #5 clk = ~clk;// clock generation
-
-    initial begin
-        reset = 1;
-        #5;
-        reset =0;
-    end
-
-    mem_if vif (clk,reset);
-
-    mem_ram DUT u1 (.clk(vif.clk),
-                    .reset(vif.reset),
-                    .addr(vif.addr),
-                    .wdata(vif.wdata),
-                    .rdata(vif.rdata),
-                    .wr_en(vif.wr_en),
-                    .rd_en(vif.rd_en)
-    )
-
-    initial begin
-        uvm_config_db #( virtual mem_if)::set( uvm_root::get(),"*","vif",vif);
-        run_test ();
-    end
-endmodule
-
-class my_base_pkt extends uvm_sequence_item;
-    
-    rand bit [31:0] addr;
-    rand bit [31:0] data;
-    
-    `uvm_object_utils_begin (my_base_pkt)
-        `uvm_field_int (data, UVM_ALL_ON)
-        `uvm_field_int (addr, UVM_ALL_ON)
-    `uvm_object_utils_end
-
-    function new(string name= "my_base_pkt");
-        super.new (name);
-    endfunction
-    constraint addr_range {
-        addr inside {[32'h00000000: 32'h60000000]};
-    };
-
-endclass
-class my_seq_item extends uvm_sequence_item;
-    rand bit [7:0] addr;
-    rand bit [7:0] wdata;
-    rand bit  wr_en;
-    rand bit  rd_en;
-         bit [7:0] rdata;
-
-
-    `uvm_object_utils_begin (my_seq_item)
-        `uvm_field_int (addr,UVM_ALL_ON);
-        `uvm_field_int (addr,UVM_ALL_ON);
-        `uvm_field_int (addr,UVM_ALL_ON);
-        `uvm_field_int (addr,UVM_ALL_ON);
-        `uvm_field_int (addr,UVM_ALL_ON);
-    `uvm_object_utils_end
-
-
-    function new (string name = "my_seq_item") 
-        super.new(name);
-    endfunction
-
-
-    constraint wr_rd {
-        wr_en!=rd_en;
-    };
-endclass
-
 class my_tx #(d_width = 16,a_width = 16) extends uvm_sequence_item;
 
     rand bit [31:0] addr;
@@ -208,29 +18,6 @@ class my_tx #(d_width = 16,a_width = 16) extends uvm_sequence_item;
         addr % 1000 ==0;
     }
 endclass
-
-class my_seq extends uvm_sequence #(my_seq_item);
-    `uvm_object_utils (my_seq);
-
-    my_seq_item txns;
-    int N=30;
-
-    function new (string= "name")
-     super.new (name);
-    endfunction
-
-
-
-    task body();
-        repeat (N)  begin
-            txns = my_seq_item::type_id::create ("txns");
-            start_item (txns);
-            txns.randomize();
-            finish_item(txns);
-        end
-    endtask:body
-endclass
-
 
 class my_seq extends from uvm_sequence #(my_tx);
     my_tx #(d_width,a_width)item;
@@ -278,6 +65,197 @@ class my_wr_seq #(d_width=16, a_width=16)extends uvm_sequence;
     endtask
 endclass
 
+
+class my_sequencer extends uvm_sequencer;
+    `uvm_component_param_utils (my_sequencer);
+
+    function new (string name = "my_sequencer", uvm_component parent);
+        super.new (name,parent);
+    endfunction
+endclass
+
+
+class my_driver extends uvm_driver #(my_tx #( d_width=32,a_width=32));
+    `uvm_component_utils ("my_driver");
+
+    uvm_seq_item_pull_port #(REQ,RSP)seq_item_pp1;
+    uvm_analysis_port #(RSP) axi_write_response_port;
+    uvm_tlm_analysis_fifo #(my_tx) axi_anal_fifo_h;
+
+    REQ req_wr, req_rd;
+    RSP rsp_wr, rsp_rd;
+
+    function new (string name = "my_driver", uvm_component parent);
+        super.new (name,parent);
+
+        seq_item_pp1 = new ("seq_item_pp1", this);
+
+    endfunction
+
+
+
+//  BUILD BUCKET
+    function void build_phase (uvm_phase phase);
+   
+    endfunction
+
+    function void end_of_elaboration_phase();
+    
+    endfunction
+
+    function void start_of_simulation_phase();
+    endfunction
+
+    
+    function void connect_phase();
+    endfunction
+//
+
+// RUN BUCKET
+
+    task run_phase(uvm_phase phase);
+     phase.raise_objection(this);
+
+
+
+    phase.drop_objection(this)
+    // pre_reset
+    //reset
+    //post_reset
+
+    //pre_configure
+    //configure
+    //post_configure
+
+    //pre_main
+    //main
+    //post_main
+
+    //pre_shutdown
+    //shutdown
+    //post_shutdown
+
+
+    endtask
+// 
+
+// CLEAN_UP BUCKET
+
+    function void extract_phase();
+    endfunction
+
+    function void check_phase();
+    endfunction
+    
+    function void report_phase();
+    endfunction
+
+//
+endclass
+
+
+
+// DownCast & Upcase
+
+//UPCASTING Treating a child object as parent type
+
+class base_pkt extends uvm_sequence_item;
+endclass
+
+class burst_pkt extends base_pkt;
+endclass
+
+burst_pkt bp;
+base_pkt p;
+
+bp = new("bp");
+p = bp; //upcasting
+
+uvm_component comps [$];
+drv = my_driver::type_id::create ("drv");
+comps.pushback(drv);
+
+/*
+why do we ever need this ?
+ans - for a number of reasons
+1) you can use a generic base component handle and still be able to call the most derived method.
+2) factory always returns the base handle type but the actual object is derived type.
+3) phase execution
+4) reuseablitiy
+*/
+
+
+// DURING UPCASTING YOU CANNOT USE A FUNCTION THAT IS EXCLUSIVELY IN THE DERIVED CLASS AND NOT OVERRIDEEN for that we need to downcast.
+// DOWNCAST Gives you the ability to check that whether this parent handle was upcasted before, ie does it really point to a child? if yes then you can safely use all the derived-only methods from the child
+p = burst_pkt::type_id::create("p");
+bp = p // illegal in SV
+if (!$cast (bp, p))
+begin
+    `uvm_error("nope")
+end
+
+
+class parent;
+    var x;
+    virtual function vir;
+    // some code
+    endfunction
+
+    function non_vir_parent
+    endfunction
+endclass
+
+class child extends from parent;
+    var y;
+    virtual fuction vir;
+    //somecode
+    endfunction
+
+    function non_vir_parent
+    endfunction
+
+    function fun_child;
+   // some code
+    endfunction
+endclass
+
+
+parent p;
+child c;
+c = new ("c");
+p =c  //  UPCASTING
+/*
+now this means that p.vir would actually execute function from child and not parent.
+but p.non_vir_parent will actually execute that function from parent.
+and p.fun_child is impossible as it is derived only ?
+and p.y can we use this var from child - cannot
+*/
+parent p;
+child c;
+
+$cast(c,p); // downcasting
+// ye cast do kaam karta hai, 1) check karega ki parent kya sach me child ko point karta hai ? agaar haan to tumhe ek child ka handle dedega jo seedha child ko point karta hai to use child only methods.
+//then now if i do p.vir - same as before child's derived function, but now i can also do c.fun_child or access c.y which are both child only functions right ?
+
+class my_base_pkt extends uvm_sequence_item;
+    
+    rand bit [31:0] addr;
+    rand bit [31:0] data;
+    
+    `uvm_object_utils_begin (my_base_pkt)
+        `uvm_field_int (data, UVM_ALL_ON)
+        `uvm_field_int (addr, UVM_ALL_ON)
+    `uvm_object_utils_end
+
+    function new(string name= "my_base_pkt");
+        super.new (name);
+    endfunction
+    constraint addr_range {
+        addr inside {[32'h00000000: 32'h60000000]};
+    };
+
+endclass
+
 class my_sequence extends  uvm_sequence #(my_base_pkt);
     `uvm_object_utils(my_sequence)
     my_base_pkt item;
@@ -297,11 +275,13 @@ class my_sequence extends  uvm_sequence #(my_base_pkt);
     endtask:body
 endclass
 
-class some_sequencer extends uvm_sequencer#(my_seq_item);
-    `uvm_component_utils (some_sequencer);
-    function new (string name= " some_seuqnecer", uvm_component parent);
-        super.new(name,parent);
+class my_sequencer extends uvm_sequence #(my_base_pkt);
+    `uvm_component_utils (my_sequencer) // -- important for factory creation.
+
+    function new (string name="my_sequencer", uvm_component parent);
+        super.new(name, parent) // does multiple things, readies this object for phases, registers with factory, config db and report server, makes it a uvm hierarichal object. name used for hierarichal paths.
     endfunction
+
 endclass
 
 class my_driver extends uvm_driver #(my_base_pkt);
@@ -367,35 +347,6 @@ class my_driver extends uvm_driver #(my_base_pkt);
 
     function void check_phase();
     endfunction
-endclass
-
-
-class my_env extends  uvm_env;
-    `uvm_component_utils (my_env)
-    my_scoreboard myscrbrd;
-    my_agent agnt;
-    my_coverage_collector cvrge;
-
-
-    function new (string name = "my_env", uvm_component parent);
-        super.new (name,parent);  
-    endfunction
-
-
-    function void build_phase (uvm_phase phase);
-        super.new(phase);
-        myscrbrd = my_scoreboard::type_id::create ("myscrbrd",this);
-        agnt = my_agent::type_id::create ("agnt",this);
-        cvrge = my_cvrge::type_id::create ("cvrge",this);
-    endfunction
-
-    function void connect_phase (uvm_phase phase);
-        super.new (phase);
-        my_agent.read_port.connect(myscrbrd.read_port_export);
-        my_agent.write_port.connect(myscrbrd.write_port_export);
-        my_agent.write_port.connect(my_coverage_collector.analysis_export);
-    endfunction
-                            
 endclass
 
 class my_agent extends uvm_agent;
@@ -469,6 +420,37 @@ class my_monitor extends uvm_monitor;
         end
     endtask
 endclass
+
+
+class my_env extends from uvm_env;
+    `uvm_component_utils (my_env)
+    my_scoreboard myscrbrd;
+    my_agent agnt;
+    my_coverage_collector cvrge;
+
+
+    function new (string name = "my_env", uvm_component parent);
+        super.new (name,parent);  
+    endfunction
+
+
+    function void build_phase (uvm_phase phase);
+        super.new(phase);
+        myscrbrd = my_scoreboard::type_id::create ("myscrbrd",this);
+        agnt = my_agent::type_id::create ("agnt",this);
+        cvrge = my_cvrge::type_id::create ("cvrge",this);
+    endfunction
+
+    function void connect_phase (uvm_phase phase);
+        super.new (phase);
+        my_agent.read_port.connect(myscrbrd.read_port_export);
+        my_agent.write_port.connect(myscrbrd.write_port_export);
+        my_agent.write_port.connect(my_coverage_collector.analysis_export);
+    endfunction
+
+                                                  
+endclass
+
 class my_coverage_collector extends uvm_subscriber #(my_base_pkt);
     `uvm_component_utils(my_coverage_collector);
 
@@ -503,7 +485,9 @@ class my_coverage_collector extends uvm_subscriber #(my_base_pkt);
     endfunction
 endclass
 
-class my_scoreboard extends uvm_subscriber #(my_base_pkt);
+
+
+class my_scoreboard extends #uvm_subscriber #(my_base_pkt);
 // SPLIT INTO EVALUATOR AND PREDICTOR
     `uvm_component_utils (my_scoreboard)
     
@@ -600,88 +584,3 @@ class my_evaluator extends uvm_subscriber #(my_base_pkt);
 
     endfunction
 endclass
-
-
-
-
-
-// ARBITRATION SEQUENCE
-    class my_seq #(d_width = 16, a_width = 16) extends uvm_sequence_item #(my_tx #(d_width, a_width));
-    /* can have set_priorty()
-            grab ()
-            lock ()
-            ungrab ()
-            unlock ()
-    */
-
-
-        `uvm_object_param_utils (my_seq #(d_width,a_width));
-        my_tx item;
-        int no_of_tx;
-        test_config tst_cfg;
-        function new (string name = "my_seq") ;
-            super.new (my_seq);
-            if (!uvm_config_db #(test_config)::get( NULL, "*", "test_config", tst_cfg )) `uvm_fatal (get_type_name(), "FAILED TO GET TEST CONFIG" );
-            tst_cfg.no_of_tx = no_of_tx;  
-        endfunction
-        task body ();
-            item = my_tx#(d_width,a_width)::type_id::create("item");
-            repeat (no_of_tx) begin
-                start_item (item);
-                    assert (item.randomize() with b_type==FIXED);
-                finish_item(item);
-            end
-
-        endtask
-    endclass
-
-
-
-    class my_sequencer #(d_width = 16, a_width=16) extends uvm_sequencer  #(item(d_width,a_width));
-        `uvm_component_param_utils ("my_sequencer");
-
-        function new (string name =  "my_sequencer", uvm_component parent);
-            super.new (name,parent);
-        endfunction
-
-
-        function void build_phase (uvm_phase phase);
-            super.new (phase);
-            this.arb_mode - UVM_SEQ_ARB_FIFO;
-            this.
-
-        endfunction
-
-
-    /*
-    can have arbitration modes,
-    1) UVM_SEQ_ARB_FIFO
-    2) UVM_SEQ_ARB_STRICT_FIFO
-    3) UVM_SEQ_ARB_WEIGHTED_FIFO
-    4) UVM_SEQ_ARB_RANDOM
-    */ 
-
-    endclass
-
-
-    class my_arb_seq#(a_width=16, d_width=16) extends uvm_sequence #(d_width,a_width);
-        `uvm_object_param_utils (my_arb_seq #(d_width, a_width));
-
-        function new (string name="my_arb_seq");
-            super.new (name);
-        endfunction
-        virtual task pre_start ();
-            super.pre_start();
-            set_priorty(200);
-        endtask
-
-
-        task body ();
-
-        endtask
-
-
-    endclass
-/// ARBITRATION SEQUENCE
-
-
